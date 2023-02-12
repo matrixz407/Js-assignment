864711079430b6a3c6fefa990b2588293952ae64
Olaniyan Temidayo <polcomteobal407@gmail.com>
2023-02-12 13:04:36 +0100

    Js assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+//5 variables with data type number
+
+let data = 100;
+
+let myDate = 10;
+
+let myDateType = 1000;
+
+let powerDate = 200;
+ 
+let myPowerDate = 2000;
+
+
+//5 variables with data type string
+
+
+let wishList ='milliondollar';
+
+let digitalVal = "multitalented";
+
+let myFavourite = 'venza';
+
+let myShowcase = "superstar";
+
+let endShow = "viewCenter";
+
+
+
+//5 variables with data type arrays
+
+
+let myNumbers = ['2','4','5']
+
+let myShoppingListItem = ['rice', 'beans', 'meat']
+
+let studyRoom = ['vase', 'books', 'pens']
+
+let valGift = ['chocolate', 'wine', 'flowers']
+
+let dreamCar = ['spider', 'crosstour']
+
+
+
+//lists of javascript data type
+
+
+
+strings, number, boolean, array, null, undefined 
+
+
+
+
